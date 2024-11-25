2b28e4e0c1c44568d9494cf18887990006b62e59
Haopeng <haopeng138@gmail.com>
2024-11-25 14:03:11 +0100

    vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import lit from "@astrojs/lit";
-
+import vercelServerless from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://haopeng138.github.io', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   base: '/astro-gym',
   sitemap: true, // Generate sitemap (set to "false" to disable)
   integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
+  output: 'server',
+  adapter: vercelServerless(),
 });
